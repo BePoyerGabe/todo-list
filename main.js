5951d99b18ca475b612aac6b5f58fa380f7be927
BePoyerGabe <bernardopoyer00@gmail.com>
2021-01-05 14:03:10 -0300

    only css and js

---
+++ main.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#taskField')
+const todos = document.querySelector('.todos')
+
+
+document.querySelector('.add').addEventListener('click', () => {
+    const taskDiv = document.createElement('div')
+    const taskTitle = document.createElement('h2')
+    const paragraph = document.createElement('p')
+    const button = document.createElement('button')
+
+    
+    let today = new Date()
+    today = Intl.DateTimeFormat('pt-BR').format(today)
+
+    todos.appendChild(taskDiv)
+    
+    let newTask = input.value
+    taskTitle.textContent = newTask
+    taskDiv.appendChild(taskTitle)
+
+    paragraph.textContent = `Adicionada em ${today}`
+    taskDiv.appendChild(paragraph)
+
+    button.textContent = "Done"
+    button.classList.add('removeTask')
+    taskDiv.appendChild(button)
+
+    button.addEventListener('click', ()=> {
+    
+    })
+
+})
